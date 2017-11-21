6cf5ec38764db8a81bec0994415c06286a2c2b2f
xxxsimons <974393900@qq.com>
2017-11-21 22:30:34 +0800

    change axios to fetch

--- Wei_Components/Comment/Comment.js
+++ Wei_Components/Comment/Comment.js
@@ -1,6 +1,5 @@
 import React, {Component} from  'react'
 import ReactDOM from 'react-dom';
-import axios from 'axios';
 import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 
 class Comment extends Component{
@@ -17,69 +16,71 @@ class Comment extends Component{
 
     }
     componentDidMount() {
-        axios.get('http://119.28.24.179:8081/comments?page=0')
-            .then((response) => {
-                console.log(response)
-                const comments = response.data.data
-                this.setState({
-                    comments
+        fetch('http://119.28.24.179:8081/comments?page=0',{
+            method:'get'
+        })
+            .then(response=>{
+                response.json().then(result=>{
+                    const comments = result.data
+                    this.setState({
+                        comments
+                    })
                 })
-            }).catch((error) => {
-            console.log(error)
+            })
+        fetch('https://bird.ioliu.cn/v1?url=http://119.28.24.179:8081/page',{
+            method:'get'
         })
-        axios.get('https://bird.ioliu.cn/v1?url=http://119.28.24.179:8081/page')
-            .then((response)=>{
-                console.log(response)
-                const page = response.data.count
-                // const pages = this.state.pages
-                // for (let i=0;i<page;i++){
-                //     pages.push(1)
-                // }
-                this.setState({page})
-            }).catch((error)=>{
-            console.log(error)
-        })
+            .then(response=>{
+                response.json().then(result=>{
+                    const page = result.count
+                    this.setState({page})
+                })
+            })
     }
     handleClick = () =>{
         const user_name= this.refs.user_name.value.trim()
         const comment_content= this.refs.comment_content.value.trim()
-        axios.post('https://bird.ioliu.cn/v1?url=http://119.28.24.179:8081/comment',{
-            user_name:user_name,
-            comment_content:comment_content,
-            user_ID:123123
-        })
-            .then((response)=>{
-            console.log(response)
-                if (response.data.status==true){
+        fetch('https://bird.ioliu.cn/v1?url=http://119.28.24.179:8081/comment',{
+            method:'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                user_name: user_name,
+                comment_content: comment_content,
+                user_ID:123123
+            })
+        }).then(response=>{
+            response.json().then(result=>{
+                if (result.status==true){
                     alert("发表成功！")
                     //如果在最后一页，用户发表评论，则获取最后一页的评论数据，更新显示
-                    axios.get('http://119.28.24.179:8081/comments?page='+this.state.current_page)
-                        .then((response) => {
-                            console.log(response)
-                            const comments = response.data.data
-                            this.setState({
-                                comments
-                            })
-                        }).catch((error) => {
-                        console.log(error)
+                    fetch('http://119.28.24.179:8081/comments?page='+this.state.current_page,{
+                        method:'get'
+                    }).then(response=>{
+                        response.json().then(result=>{
+                            const comments = result.data
+                            this.setState({comments})
+                        })
                     })
                 }
                 else alert("发表失败！")
-        }).catch((error)=>{
-            console.log(error)
-    })
+            })
+        })
     }
     //分页功能
     changePage = (e,index) =>{
-        axios.get('http://119.28.24.179:8081/comments?page='+index)
-            .then((response) => {
-                const comments = response.data.data
+        fetch('http://119.28.24.179:8081/comments?page='+index,{
+            method:'get'
+        }).then(response=>{
+            response.json().then(result=>{
+                const comments = result.data
                 this.setState({
                     comments,
                     current_page:index
                 })
-            }).catch((error) => {
-            console.log(error)
+            })
         })
     }
     render(){
