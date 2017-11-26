dd97c7da99970a1852f86c3f99b0ed0339098505
alone <1157450536@qq.com>
2017-11-26 22:16:15 +0800

    修改OpenxyComponent

--- Dong_Components/indexInstance/my-app/src/OpenxyComponent.js
+++ Dong_Components/indexInstance/my-app/src/OpenxyComponent.js
@@ -1,15 +1,12 @@
-import OpenxyCarousel from 'OpenxyCarousel';
-import OpenxyClassInfo from 'OpenxyClassInfo';
-import OpenxyFooter from 'OpenxyFooter';
-import OpenxyLearning from 'OpenxyLearningLayout';
-import OpenxyNav from 'OpenxyNav';
-import OpenxySubfield from 'OpenxySubfield';
-import Layout from 'Layout'
+import OpenxyCarousel from 'OpenxyCarousel';//轮播图
+import OpenxyClassInfo from 'OpenxyClassInfo';//课程展示
+import OpenxyFooter from 'OpenxyFooter';//页脚
+import OpenxyLearning from 'OpenxyLearningLayout';//学习页面布局
+import OpenxyNav from 'OpenxyNav';//导航栏
+import OpenxySubfield from 'OpenxySubfield';//分栏
+import Layout from 'Layout';//首页布局
 
-export default OpenxyCarousel;
-export default OpenxyClassInfo;
-export default OpenxyNav;
-export default OpenxyFooter;
-export default OpenxyLearning;
-export default OpenxySubfield;
-export default Layout;
+export default
+{
+    OpenxyCarousel,OpenxyClassInfo,OpenxyNav,OpenxyFooter,OpenxyLearning,OpenxySubfield,Layout
+}
